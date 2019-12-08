27f890af0f6c33225fdd13d8f9439b9bc29c7643
318Thorne <c3186564@uon.edu.au>
2019-12-08 16:44:20 +1100

    gatsby set up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: "William Thorne",
+    description: "William Thorne",
+    author: "William Thorne",
   },
   plugins: [
+    "gatsby-plugin-emotion",
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
