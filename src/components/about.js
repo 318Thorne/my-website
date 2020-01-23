b7a803caa19473e6c0bb78becf66ae03c550158a
318Thorne <c3186564@uon.edu.au>
2020-01-24 00:13:49 +1100

    added about component with basi skeleton and draft "about me"

---
+++ src/components/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const About = () => (
+  <article>
+    <h2>This is a draft about me</h2>
+    <p>
+      Lorem Ipsum ... I am a software developer based in Newcastle, NSW. From my
+      studies and work experience I have been fortunate enough to gain a vast
+      array of skills and strong understanding in software development using a
+      range of different technologies from great mentors (this sentence is a
+      mess I know). I am always seeking to learn more and grow as a developer
+    </p>
+  </article>
+)
+export default About
